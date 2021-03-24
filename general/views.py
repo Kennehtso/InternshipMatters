c2482bf6c6bb2a3dc5d08013fe0d7e753f84cc8f
Ken <kennethso1211@gmail.com>
2021-03-24 09:15:58 +0800

    Init - url routing & views

--- general/views.py
+++ general/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('Home page')
+
+def result(request):
+    return HttpResponse('Result page')
+
+def detail(request):
+    return HttpResponse('Detail page')
