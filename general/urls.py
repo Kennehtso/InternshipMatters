c1876f6c8563eeb67c13d077c9d7b07bcfa3996b
Ken <kennethso1211@gmail.com>
2021-03-24 09:21:58 +0800

    render page from templates with {%extends%}

--- general/urls.py
+++ general/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.home),
+    path('home/',views.home),
     path('result/',views.result),
     path('detail/',views.detail),
 ]
