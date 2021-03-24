461cf4e8b0f8fbdc01d1e73c13060bd6809fed93
Ken <kennethso1211@gmail.com>
2021-03-24 10:06:06 +0800

    Apply static files like, css, png

--- internshipMatters/settings.py
+++ internshipMatters/settings.py
@@ -118,4 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+# Customize Added
+STATICFILES_DIRS = [
+    Path.joinpath(BASE_DIR, STATIC_URL)
+]
