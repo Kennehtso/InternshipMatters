a43787d9b66642e0fcf61fc568db99b04be7cbb4
kennethso <kengame121101@gmail.com>
2021-04-10 14:59:07 +0800

    -u email credentials

--- internshipMatters/settings.py
+++ internshipMatters/settings.py
@@ -163,8 +163,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kengame121101@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('mailHost')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 
 
