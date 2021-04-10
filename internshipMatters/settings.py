23ee2cea6c211dcf235fa63ac39771a611bb6a96
kennethso <kengame121101@gmail.com>
2021-04-10 22:27:49 +0800

    Update settings.py

--- internshipMatters/settings.py
+++ internshipMatters/settings.py
@@ -19,11 +19,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+import environ
+env = environ.Env()
+environ.Env.read_env() # reading .env file
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'grr+&ow&z62lq+d($0d+2)y3uwrnd58_lwi3%cs@bvu6#gcm61'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['internships-matters.herokuapp.com', '127.0.0.1']
 
@@ -115,10 +119,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-
-import environ
-env = environ.Env()
-environ.Env.read_env() # reading .env file
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
