2fde6cd6edac3c370a043c7bb70720d887cf146b
kennethso <kengame121101@gmail.com>
2021-11-27 19:07:16 +0800

    sendgrid intergrate

--- internshipMatters/settings.py
+++ internshipMatters/settings.py
@@ -160,10 +160,11 @@ STATICFILES_DIRS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST = 'smtp.sendgrid.net'
+#EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_USER = 'apikey' 
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 
 #Whitenoise static files handling
