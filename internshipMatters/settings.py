90fa6dc43b03ba1f667c32e809ecab0cdaeeb43f
kennethso <kengame121101@gmail.com>
2022-03-03 04:01:34 +0800

    Update settings.py

--- internshipMatters/settings.py
+++ internshipMatters/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 #Static Root for collectstatic
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 if not DEBUG:
