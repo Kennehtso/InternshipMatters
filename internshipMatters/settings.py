a455e68fdb52e614d7ac2071718e429f0d6b0bd9
kennethso <kengame121101@gmail.com>
2022-03-03 03:19:06 +0800

    disable AWS S3 setting

--- internshipMatters/settings.py
+++ internshipMatters/settings.py
@@ -136,6 +136,7 @@ if not DEBUG:
     DATABASES['default'].update(env.db())
 
     #AWS S3 Connetction
+    """
     AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
@@ -148,7 +149,7 @@ if not DEBUG:
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/' #'static/'
     ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-
+    """
 # Customize Added
 STATICFILES_DIRS = [
     #BASE_DIR / "static",
